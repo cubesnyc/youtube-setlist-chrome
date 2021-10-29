ed34a086f9a9cede37ef59ef852f50761d531b49
Jody LeCompte <jody@jodylecompte.com>
2021-10-28 21:06:19 -0500

    Extract CSS into it's own module

---
+++ src/browser/styles.ts
@@ -0,0 +1,27 @@
+export const css = `
+  #yt-setlist {
+    margin-bottom: 20px;
+    font-size: 16px;
+    border: 1px solid var(--yt-spec-10-percent-layer);
+    color: var(--yt-endpoint-color, var(--yt-spec-text-primary));
+  }
+
+  #yt-setlist h2 {
+    color: var(--yt-endpoint-color, var(--yt-spec-text-primary));
+  }
+
+  #yt-setlist .yts-header {
+    padding: 10px;
+    padding-left: 20px;
+    background-color: var(--yt-spec-brand-background-primary);
+  }
+
+  #yt-setlist .yts-list {
+    padding: 10px;
+    padding-left: 20px;
+  }
+
+  #yt-setlist .yts-list a {
+    color: var(--yt-endpoint-color, var(--yt-spec-call-to-action));
+  }
+`;
